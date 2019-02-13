2acdc8ccab5f840ad36290641837a0cae73d6e40
Ricardo Ibarra <ricardo.ibarra@fox.com>
2019-02-12 18:03:43 -0600

    Pagina de privacidad

--- routes/general.js
+++ routes/general.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/privacy',function(req,res,next){
+  res.render('privacy',{title: "Privacy"});
+})
+
 module.exports = router;
